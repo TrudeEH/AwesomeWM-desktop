8601e796c640d1fff1b8006ffc82c0f17dfd2e55
TrudeEH <48379395+trudertube@users.noreply.github.com>
2020-08-16 18:48:01 +0100

    TrudeOS v1.2

--- OSconfig.sh
+++ OSconfig.sh
@@ -74,7 +74,7 @@ echo -ne '[####################.....]\r'
 # Instalar dependências do ambiente:
 sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 -y &> /dev/null
 echo -ne '[######################...]\r'
-echo -ne '\n'
+
 cd polybar && ./build.sh &> /dev/null
 cd ..
 echo -ne '[#########################]\r'
