73f0ecf677ad4a3f859761bfa9b70ed6872e7123
TrudeEH <48379395+trudertube@users.noreply.github.com>
2020-08-24 00:17:38 +0100

    TrudeOS v1.3

--- OSconfig.sh
+++ OSconfig.sh
@@ -76,7 +76,8 @@ echo -ne '[####################.....]\r'
 sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 -y &> /dev/null
 echo -ne '[######################...]\r'
 
-cd polybar && ./build.sh &> /dev/null
+git clone https://github.com/jaagr/polybar.git
+cd polybar && ./build.sh -A -a -p -n -c &> /dev/null
 cd ..
 echo -ne '[#########################]\r'
 echo -ne '\n'
