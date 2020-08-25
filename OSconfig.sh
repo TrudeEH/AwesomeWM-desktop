470604e0029d1693afd612ea1902a74610288415
trude <trude@trude-pc.lan>
2020-08-25 08:44:25 +0100

    TrudeOS v1.4

--- OSconfig.sh
+++ OSconfig.sh
@@ -7,82 +7,91 @@ sudo echo
 echo "[+] A instalar TrudeOS..."
 
 # Atualizar base do sistema:
+echo
+echo "[+] A atualizar a base do sistema..."
 echo -ne '[.........................]\r'
 sudo apt update -y &> /dev/null
-echo -ne '[#........................]\r'
-sudo apt upgrade -y &> /dev/null
-echo -ne '[##.......................]\r'
-sudo apt dist-upgrade -y &> /dev/null
-echo -ne '[###......................]\r'
-sudo apt autoremove -y &> /dev/null
-echo -ne '[####.....................]\r'
-sudo apt autoclean -y &> /dev/null
 echo -ne '[#####....................]\r'
-
+sudo apt upgrade -y &> /dev/null
+echo -ne '[##########...............]\r'
+sudo apt dist-upgrade -y &> /dev/null
+echo -ne '[################.........]\r'
+sudo apt autoremove -y &> /dev/null
+echo -ne '[#####################....]\r'
+sudo apt autoclean -y &> /dev/null
+echo -ne '[#########################]\r'
+echo -ne '\n'
+echo
 # --------------------------------------------------
 
 # Instalar pacotes:
-sudo apt install gparted kmix network-manager-gnome network-manager firefox-esr telegram-desktop flameshot vim alsa-utils wicd-curses htop git pcmanfm nano pavucontrol snapd -y &> /dev/null
-echo -ne '[#########................]\r'
-
-# Instalar python:
-sudo apt install -y python3-tk python3 python3-pip python python-pip &>/dev/null
-echo -ne '[############.............]\r'
-
-# Instalar GUI:
-sudo apt install dmenu slim xorg awesome xterm compton -y &> /dev/null
-echo -ne '[##############...........]\r'
-
-# Instalar wine:
-sudo apt install wine-stable mono-complete -y &> /dev/null
-echo -ne '[################.........]\r'
+sudo apt install gparted kmix network-manager-gnome network-manager firefox-esr telegram-desktop flameshot vim alsa-utils wicd-curses htop git pcmanfm nano pavucontrol snapd -y
+sudo apt install -y python3-tk python3 python3-pip python python-pip
+sudo apt install dmenu slim xorg awesome xterm compton -y 
+sudo apt install -y gufw macchanger proxychains
+sudo apt install wine-stable mono-complete -y 
+sudo apt-get install unifont cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 -y
 
 # Instalar wifite:
 # Bully:
+echo
+echo "[+] A instalar wifite..."
+echo -ne '[#........................]\r'
 sudo apt-get -y install reaver libpcap-dev libssl-dev aircrack-ng &> /dev/null
+echo -ne '[####.....................]\r'
 git clone https://github.com/aanarchyy/bully &> /dev/null
+echo -ne '[######...................]\r'
 cd bully*/
 cd src/
 make &> /dev/null
+echo -ne '[#######..................]\r'
 sudo make install &> /dev/null
+echo -ne '[########.................]\r'
 cd ../..
-rm -rf bully*/
+rm -rf bully*/ &> /dev/null
 sudo apt install pyrit hashcat macchanger -y &> /dev/null
+echo -ne '[##########...............]\r' 
 # HCX---
 git clone https://github.com/ZerBea/hcxdumptool.git &> /dev/null
+echo -ne '[############.............]\r' 
 cd hcxdumptool
 sudo apt-get install libcurl4-openssl-dev -y &> /dev/null
+echo -ne '[##############...........]\r' 
 sudo apt-get install libssl-dev -y &> /dev/null
+echo -ne '[###############..........]\r'
 make &> /dev/null
-sudo make install &> /dev/null
+echo -ne '[################.........]\r'
+sudo make install &> /dev/null 
+echo -ne '[#################........]\r'
 cd ..
 rm -rf hcxdumptool
 git clone https://github.com/ZerBea/hcxtools.git &> /dev/null
+echo -ne '[##################.......]\r'
 cd hcxtools
 make &> /dev/null
-sudo make install &> /dev/null
+echo -ne '[###################......]\r'
+sudo make install
+echo -ne '[####################.....]\r'
 cd ..
 rm -rf hcxtools
 # Wifite2
 git clone https://github.com/derv82/wifite2.git &> /dev/null
 cd wifite2
-sudo python3 setup.py install &> /dev/null
+echo -ne '[######################...]\r'
+sudo python3 setup.py install
+echo -ne '[########################.]\r'
 cd ..
 sudo rm -rf wifite2
 cd ~/TrudeOS
-echo -ne '[####################.....]\r'
+echo -ne '[#########################]\r'
+ecgo -ne '\n'
 
-# Instalar dependências do ambiente:
-sudo apt-get install unifont cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 -y &> /dev/null
-echo -ne '[######################...]\r'
-
+# Polybar
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf ; fc-cache &> /dev/null
-git clone https://github.com/jaagr/polybar.git &> /dev/null
-cd polybar && ./build.sh -A -a -p -n -c &> /dev/null
+git clone https://github.com/jaagr/polybar.git
+cd polybar && ./build.sh -A -a -p -n -c
 cd ..
 rm -rf polybar
-echo -ne '[#########################]\r'
-echo -ne '\n'
 
 # Configurar ambiente:
 mkdir ~/.config &> /dev/null
@@ -107,8 +116,7 @@ cp -r fonts/* ~/.local/share/fonts/
 cp config ~/.config/polybar
 cp vimrc ~/.vimrc
 
-sudo apt install -y gufw macchanger proxychains &> /dev/null
-
+echo
 echo "[I] TrudeOS foi instalado! Pode reiniciar o computador."
 
 # Notas:
